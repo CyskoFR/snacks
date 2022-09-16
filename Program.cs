d1a8519628687333db890a4c68e88ea6cca80966
CyskoFR <franc.sko91@gmail.com>
2022-09-16 16:42:19 +0200

    snack8

--- Program.cs
+++ Program.cs
@@ -58,7 +58,7 @@ Console.WriteLine($"La somma dei tuoi numeri é {sum}");
 
 Console.WriteLine("Calcolo la somma e la media dei numeri da 2 a 10");
 
-int[] arrayOfNumbers = { 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int[] arrayOfNumbers = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 int sum2 = 0;
 
@@ -77,11 +77,72 @@ Console.WriteLine("Inserisci un numero");
 
 int userNumber = Convert.ToInt32(Console.ReadLine());
 
-if(userNumber % 2 == 0)
+if (userNumber % 2 == 0)
 {
     Console.WriteLine($"Hai inserito un numero pari: {userNumber}");
 }
 else
 {
-    Console.WriteLine($"Hai inserito un numero dispari({userNumber}). Il suo successivo é: {userNumber+1}");
+    Console.WriteLine($"Hai inserito un numero dispari({userNumber}). Il suo successivo é: {userNumber + 1}");
 }
+
+
+//Snack6.
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. Chiedi all'utente il suo nome e comunicagli se puó partecipare o meno alla festa.
+
+string[] guestsList = { "mario", "francesco", "dario", "luca", "gigi", "maria", "anna" };
+
+Console.WriteLine("Il suo nome, prego...");
+
+string guestName = Console.ReadLine().ToLower();
+
+foreach (string guest in guestsList)
+{
+    if (guestName == guest)
+    {
+        Console.WriteLine("Prego. Puó entrare.");
+    }
+    else
+    {
+        Console.WriteLine("Spiacente, non é in lista.");
+        break;
+    }
+}
+
+
+//Snack7.
+//Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se é dispari inseriscilo nell'array.
+
+//int[] newArray = {};
+
+//for (int i = 0; i < 6; i++)
+//{
+//    Console.WriteLine("Inserisci un numero");
+//    int userNum = Convert.ToInt32(Console.ReadLine());
+//    if (userNum % 2 != 0)
+//    {
+//        newArray.Append(userNum);
+//    }
+//}
+
+//Console.WriteLine(newArray.ToString());
+
+
+//Snack8.
+//Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
+
+int[] intNumbersArray = { 1, 2, 5, 7, 12, 56, 21, 87, 55, 32 };
+
+int oddSum = 0;
+
+for (int i = 0; i < intNumbersArray.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{intNumbersArray[i]} ");
+        oddSum += intNumbersArray[i];
+    }
+}
+Console.WriteLine($"La somma degli elementi in posizione dispari é {oddSum}");
+
+
